25d4fd3659f6c4cd5e0cff5f597a6bdfbad7291e
Anand Vaidya <anandvaidya46@gmail.com>
2026-02-06 23:25:12 +0530

    Add files via upload

---
+++ DSA/data_types.cpp
@@ -0,0 +1,36 @@
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    /
+    //int
+    int x=10;
+    //long
+    long x=15;
+    cin >>x;
+
+    //long long
+    long long x = 15000000;
+    */
+
+/*
+    //float
+    float x = 5.6;
+    double y = 5;
+    cout <<"Value of y:" <<y; 
+    
+    return 0;
+    
+}
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //string and getline 
+    string s1;
+    string s2;
+    cin >> s1 >> s2;
+    cout <<s1 << " " << s2;
+    return 0;
+}
---
+++ DSA/getline.cpp
@@ -0,0 +1,21 @@
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    getline(cin, str);
+    cout << str;
+    return 0;
+}
+*/
+
+// Char
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char ch;
+    cin >> ch;
+    cout << ch;
+    return 0;
+}
