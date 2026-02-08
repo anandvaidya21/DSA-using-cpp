039e8ad5967e6dac94f8dc65e2ea2c3694e26f72
Anand Vaidya <anandvaidya46@gmail.com>
2026-02-08 18:57:33 +0530

    daily potd

---
+++ POTD/110.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (!root)
+            return 0;
+
+        int leftH = height(root->left);
+        int rightH = height(root->right);
+
+        return max(leftH, rightH) + 1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if (!root)
+            return true;
+
+        int leftH = height(root->left);
+        int rightH = height(root->right);
+
+        if (abs(leftH - rightH) > 1)
+            return false;
+
+        return isBalanced(root->left) && isBalanced(root->right);
+    }
+};
