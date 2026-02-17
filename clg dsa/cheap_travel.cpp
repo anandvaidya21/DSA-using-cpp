a2987d1a2ed6a2744816e4cc90fee6f2809b568e
Anand Vaidya <anandvaidya46@gmail.com>
2026-02-17 14:00:56 +0530

    Add files via upload

---
+++ clg dsa/cheap_travel.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, m, a, b;
+    cin>>n>>m>>a>>b;
+    int c1,c2,c3;
+    c1= n*a;
+    c2= (n/m)*b + (n%m)* a;
+    c3= ((n/m)+(n%m!=0?1:0))*b;
+    
+    int ans = min(c1,min(c2,c3));
+    cout<<ans;
+    
+    
+}
