a9853560c8b580f899a8f3ed4129ef1493769543
Anand Vaidya <anandvaidya46@gmail.com>
2026-02-26 10:31:06 +0530

    Add files via upload

---
+++ clg dsa/337A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin >> n>> m;
+    int a[m];
+    for(int i =0;i<m;i++)
+    {
+        cin >> a[i];
+
+    }
+    sort(a, a+m);
+    int ans= INT_MAX;
+    for(int i=0; i<=m-n;i++){
+    int differ = a[i+n-1]- a[i];
+    ans = min(ans,differ);
+    }
+    cout  << ans;
+
+}
