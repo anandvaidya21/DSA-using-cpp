ff4d5a1e1acfda492a739015b049d77fd848feaf
Anand Vaidya <anandvaidya46@gmail.com>
2026-05-03 18:10:53 +0530

    Add files via upload

---
+++ leetcode/reverse.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int RevNum = 0;
+
+        while (x != 0) {
+            int LastDigit = x % 10;
+            x = x / 10;
+
+           
+            if (RevNum > INT_MAX / 10 || RevNum < INT_MIN / 10)
+                return 0;
+
+            RevNum = RevNum * 10 + LastDigit;
+        }
+
+        return RevNum;
+    }
+};
