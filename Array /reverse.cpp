b58acb9d360e9fdecf19dde0d9f3fac87c5ec41f
Anand Vaidya <anandvaidya46@gmail.com>
2026-09-24 11:32:57 +0530

    Create reverse.cpp

---
+++ Array /reverse.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+    void reverse(int arr[], int n){
+        int i = 0; 
+        int j = n-1;
+
+        while(i<j){
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+        
+    }
+};
